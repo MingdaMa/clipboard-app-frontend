cc2a094a9199211e3b9473ff3ca15409ceb01453
Mingda Ma <mma24@dons.usfca.edu>
2023-11-06 22:43:35 -0800

    changed url

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export const API_BASE_URL = 'https://clipboard-app-backend-auth-5258f7d49733.her
 export const ACCESS_TOKEN = 'accessToken';
 
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:5173/oauth2/redirect'
-export const OAUTH2_REDIRECT_URI = 'https://clipboard-app-frontend-auth-d66e7d988eb5.herokuapp.com/'
+export const OAUTH2_REDIRECT_URI = 'https://clipboard-app-frontend-auth-3fd8f053842d.herokuapp.com/ '
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
